4e871f26d4b089f34f636c230d81d41562204fce
Daniel Sinclair <d@niel.nyc>
2021-08-23 17:22:11 -0400

    added convert flag

--- index.js
+++ index.js
@@ -21,22 +21,24 @@ const getInput = () => {
   const graph = core.getInput('graph')
   const variant = core.getInput('variant')
   const federated = core.getInput('federated')
+  const convert = core.getInput('convert')
   const subgraph = core.getInput('subgraph')
   if (federated && !subgraph) throw new Error('federated graph requires subgraph input')
   const path = core.getInput('path')
   const artifact = core.getInput('artifact')
   if (!path && !artifact) throw new Error('path or artifact inputs are required')
   const routingURL = core.getInput('routing_url')
-  return { graph, variant, federated, subgraph, path, artifact, routingURL }
+  return { graph, variant, federated, convert, subgraph, path, artifact, routingURL }
 }
 
 async function run() {
   try {
-    const { graph, variant, federated, subgraph, path, artifact, routingURL } = getInput()
+    const { graph, variant, federated, convert, subgraph, path, artifact, routingURL } = getInput()
     const schema = artifact ? await downloadArtifact(artifact) : path
     const args = ['--schema', schema]
     if (federated) args.push('--name', subgraph)
     if (routingURL) args.push('--routing-url', routingURL)
+    if (convert) args.push('--convert')
     await rover([
       federated ? 'subgraph' : 'graph',
       'publish',
